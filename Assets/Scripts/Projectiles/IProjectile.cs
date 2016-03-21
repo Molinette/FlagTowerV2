eb77fd8c06e90cdfb35b9f64ef156bdeed015dcc
Julien Lépine <lepi.julien@gmail.com>
2016-03-21 19:09:30 -0400

    hiérarchie des projectiles (sans l'interface IProjectile)

---
+++ Assets/Scripts/Projectiles/IProjectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Interface des projectiles
+ * Les différents projectiles devront retourner leur dommage
+ */
+
+public interface IProjectile : MonoBehaviour{
+	
+	public float projectileDamage();
+}
