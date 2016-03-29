b00a8b9e719253f87b41d5cdfe038a6b400460b4
tonyown1 <ant.hakim.stud@gmail.com>
2016-03-29 15:01:22 -0400

    Commit SpawnManager

--- Assets/Scripts/Enemies/EnemySpawn.cs
+++ Assets/Scripts/Enemies/EnemySpawn.cs
@@ -3,18 +3,27 @@ using System.Collections;
 
 public class EnemySpawn : MonoBehaviour {
 
-	public GameObject enemy;
+
+	public Transform [] differentSpawnPoints;
+	public GameObject [] enemies;
+
 	public float SpawnTime;
 
 	// Use this for initialization
 	void Start () {
+		
 		StartCoroutine (Spawn());
 	}
 
 	IEnumerator Spawn() {
 		while(true) {
-			Instantiate (enemy, transform.position, Quaternion.identity);
+			Transform location = getRandomSpawnPoint ();
+			Instantiate (enemies[0], location.position, Quaternion.identity);
 			yield return new WaitForSeconds (SpawnTime);
 		}
+	}
+
+	Transform getRandomSpawnPoint() {
+		return differentSpawnPoints[Random.Range (0, differentSpawnPoints.Length)];
 	}
 }
