d01dfcb4b16fb59b17a86909ee3c7268b577b299
tonyown1 <ant.hakim.stud@gmail.com>
2016-03-29 14:52:36 -0400

    Commit Importation folder enemies avec scripts et prefabs Commit Duplication de la scene principale

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : EnemyBehaviour, IDamage {
+
+	public float damage; 
+	public float health;
+	public string name;
+	//Target to follow
+	public Transform target;
+	public Animator animation;
+
+	public void Damage (float dmg) {
+
+	}
+}
