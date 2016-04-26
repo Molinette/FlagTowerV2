d940a02f1554fd656d1ec833c0d142bc2718c1e5
Antoine <an25270@ens.etsmtl.ca>
2016-04-26 08:53:34 -0700

    AirStriker creer. Mouvement pas encore fait. Droppement de bombe pas encore fait. Droppement de Runners/Bombers pas encore fait.

--- Assets/Scripts/Enemies/AirStriker.cs
+++ Assets/Scripts/Enemies/AirStriker.cs
@@ -2,5 +2,40 @@
 using System.Collections;
 
 public class AirStriker : Enemy {
+	
+	public GameObject [] ObjectsToDrop;
+	public Transform [] Targets;
+	public int GameObjectToSpawn;
 
+	private float SpawnTime = 3f;
+	private int targetDirection = GetTargetDirection;
+
+	void Start () {
+		StartCoroutine (Drop());
+	}
+
+	void Update () {
+		base.target = Targets [targetDirection];
+		if (targetDirection == 1) {
+			Move (1);
+		} else {
+			Move (-1);
+		}
+	}
+
+	IEnumerator Drop () {
+		while (true) {
+			Instantiate (ObjectsToDrop[GameObjectToSpawn], transform.FindChild ("DropAnchor").position, Quaternion.identity);
+			yield return new WaitForSeconds (SpawnTime);
+		}
+	}
+
+	private int GetTargetDirection () {
+		float x = transform.position.x;
+		if (x < 0) { //The airstrike spawned on the left.
+			return 1;
+		}
+		return 0; //The airstrike spawned on the right.
+	}
+			
 }
