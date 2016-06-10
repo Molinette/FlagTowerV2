8df8f3a4b6bc8c0389e29372bfee0e2ecee18718
Marc-Alexandre Monette Molina <marc-alexandre.monette-molina.1@ens.etsmtl.ca>
2016-06-10 13:58:16 -0400

    bombe airstriker

---
+++ Assets/AirStrikerBomb.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirStrikerBomb : MonoBehaviour {
+
+	public float damage;
+	public float timer;
+	public float explosionRadius;
+	public GameObject explosionPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		timer -= Time.deltaTime;
+		if(timer <= 0){
+			Explode();
+			GameObject.Instantiate(explosionPrefab,transform.position,Quaternion.Euler(explosionPrefab.transform.eulerAngles));
+			Destroy(gameObject);
+		}
+	}
+
+	void Explode(){
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position,explosionRadius);
+		foreach(Collider2D collider in colliders){
+			if(collider.CompareTag("Tower")){
+				collider.transform.parent.GetComponent<TowerDownScript>().changeHealth(-damage);
+			}
+		}
+	}
+}
