a7006af5fa0a7313de895098bb23ddba481eba49
Marc-Alexandre Monette Molina <marc-alexandre.monette-molina.1@ens.etsmtl.ca>
2016-06-18 12:58:00 -0400

    more balancing + sprites

---
+++ Assets/flipCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flipCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.mousePosition.x > Camera.main.WorldToScreenPoint(transform.position).x){
+			transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x),transform.localScale.y);
+		}
+		else{
+			transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x)*-1,transform.localScale.y);
+		}
+
+	}
+}
